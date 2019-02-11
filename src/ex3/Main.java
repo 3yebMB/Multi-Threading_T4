50b2ff6b32e939964a713b98c08109d8f14fc731
3yebmb <3yebmb@gmail.com>
2019-02-11 22:46:33 +0500

    don't complete ex3

--- src/ex3/Main.java
+++ src/ex3/Main.java
@@ -1,4 +1,20 @@
 package ex3;
 
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Enumeration;
+
 public class Main {
+    public static void main(String[] args) {
+        ArrayList<FileInputStream> al = new ArrayList<>();
+        for (int i = 0; i < 5; i++) {
+            try {
+                al.add(i, new FileInputStream("file_" + (i + 1) + ".txt"));
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+            Enumeration<FileInputStream> e = Collections.enumeration(al);
+        }
+    }
 }
